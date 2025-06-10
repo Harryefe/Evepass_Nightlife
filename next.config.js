b362d5624d21df42f47ed95c2c83bf6c4892a134
Harryefe <118858230+Harryefe@users.noreply.github.com>
2025-06-11 00:35:52 +0100

    Fix Next.js configuration for server-side rendering

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { 
     unoptimized: true 
   },
-  trailingSlash: true,
-  distDir: 'out',
 };
 
 module.exports = nextConfig;
